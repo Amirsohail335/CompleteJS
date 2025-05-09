22cdd3d715b483e7d3ffc4969f4770de86faf04b
Amirsohail335 <amirsohail335@gmail.com>
2025-05-09 19:20:28 +0530

    fundamental part-2

--- 02-Fundamentals-part-2/script.js
+++ 02-Fundamentals-part-2/script.js
@@ -1,1 +1,12 @@
 "use strict";
+
+function juicermaker(apple, orange) {
+  console.log(apple, orange);
+  const juice = `Make apple ${apple} and orange ${orange} by mixing`;
+  return juice;
+}
+
+juicermaker(4, 5);
+const reqJuice = juicermaker(4, 5);
+
+console.log(reqJuice);
