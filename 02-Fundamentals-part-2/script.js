35beaa2e46c927b6c287d524b21f20eaf938befb
Amirsohail335 <amirsohail335@gmail.com>
2025-05-10 13:44:20 +0530

    fundamental part-2

--- 02-Fundamentals-part-2/script.js
+++ 02-Fundamentals-part-2/script.js
@@ -10,3 +10,19 @@ juicermaker(4, 5);
 const reqJuice = juicermaker(4, 5);
 
 console.log(reqJuice);
+
+const requiredPieces = function (cutpieces) {
+  return 4 * cutpieces;
+};
+
+const fruitProcessor = function (apple, orange) {
+  console.log(orange, apple);
+  const applePieces = requiredPieces(apple);
+  const orangePieces = requiredPieces(orange);
+
+  const juice = `For making juices ${applePieces} apple pieces and ${orangePieces} orange pieces required`;
+  return juice;
+};
+
+const fruitJuice = fruitProcessor(4, 5);
+console.log(fruitJuice);
